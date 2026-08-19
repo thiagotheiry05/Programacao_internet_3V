d69d58e34697606b68ba7a8bc752d7687c433654
thiagotheiry05 <thiago.thierry.17@gmail.com>
2026-08-19 17:48:42 -0300

    Subindo as primeiras questoes

---
+++ Listas/questao01.php
@@ -0,0 +1,11 @@
+<?php 
+
+$numero = 1;
+
+while ($numero <= 50) {
+    echo $numero . ", ";
+    $numero++;
+}
+
+echo "Fim da contagem.";
+?>
---
+++ Listas/questao2.php
@@ -0,0 +1,13 @@
+<?php 
+
+for ($i = 1; $i <= 30; $i++) {
+
+    if ($i % 2 == 0) {
+        echo "$i - Par\n";
+    } else {
+        echo "$i - Ímpar\n";
+    }
+
+}
+
+?>
